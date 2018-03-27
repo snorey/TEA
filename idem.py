42f3c03b971011e9ec779d529ec0a05f22a61da3
snorey <samueljhenderson@gmail.com>
2018-03-27 16:58:05 -0500

    fixed typo in idem.py

--- idem.py
+++ idem.py
@@ -21,8 +21,22 @@ import urllib
 import urllib2
 import xml.parsers.expat
 
-import idem_settings
-from idem_settings import *
+lakezips = ["46303", "46307", "46308", "46311", "46312", "46319", "46320",
+            "46321", "46322", "46323", "46324", "46325", "46327", "46342", "46355",
+            "46356", "46373", "46375", "46376", "46377", "46394", "46401", "46402",
+            "46403", "46404", "46405", "46406", "46407", "46408", "46409", "46410",
+            "46411"]
+
+downloadzips = ["46303", "46307", "46308", "46311", "46312", "46319", "46320",
+                "46321", "46322", "46323", "46324", "46325", "46327", "46342", "46355",
+                "46356", "46373", "46375", "46376", "46377", "46394", "46401", "46402",
+                "46403", "46404", "46405", "46406", "46407", "46408", "46409", "46410",
+                "46411"]
+
+maindir = "/home/sam/TEA/TEA"
+permitdir = os.path.join(maindir, "Permits")
+enforcementdir = os.path.join(maindir, "Enforcement")
+innddir = os.path.join(maindir, "INND")
 
 class TotalUpdater:
     def __init__(self):
@@ -56,7 +70,7 @@ class ZipCollection(list):
         for zip in zips:
             updater = ZipUpdater(zip)
             self.append(updater)
-            for facility in uin tpdater.facilities:
+            for facility in updater.facilities:
                 self.facilities.add(facility)
                 self.iddic[facility.vfc_id] = facility
                 self.namedic[facility.vfc_name].append(facility)
@@ -160,6 +174,7 @@ class ZipUpdater:
         self.offline = True
 
     def do_zip(self, startfrom=False):
+        zip = self.zip
         firsttime = self.firsttime
         self.html = ""  # reset
         if self.whether_update_zip_info is True:
@@ -408,14 +423,14 @@ class ZipUpdater:
                 continue
             else:
                 try:
-                    lat, lon, googleaddress = coord_from_address(facility.full_address)
+                    lat, long, googleaddress = coord_from_address(facility.full_address)
                 except TypeError, e:  # returned False?
                     print str(e)
                     pass
                 else:
                     print facility.vfc_name, facility.full_address
                     print googleaddress
-                    facility.latlong = (float(lat), float(lon))
+                    facility.latlong = (float(lat), float(long))
                     facility.address = googleaddress
                     time.sleep(.1)
 
@@ -712,7 +727,7 @@ def generate_type_url(id, type):
 
 
 def coord_from_address(address):
-    apikey = idem_settings.google_maps_key
+    apikey = "AIzaSyBx0D5K2ae5_HxKYU_VdyghlCOFsIHya08"
     url = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s"
     url = url % (urllib.quote(address), apikey)
     apipage = urllib2.urlopen(url).read()
@@ -726,6 +741,204 @@ def coord_from_address(address):
     longitude = str(round(float(longitude), 5))
     googleadd = apipage.split('"formatted_address"')[1].split('"')[1].split('"')[0].strip()
     return (latitude, longitude, googleadd)
+
+
+### enforcement
+
+def fetch_enforcements(county="Lake", days=90, directory=enforcementdir):
+    updates = []
+    today = datetime.date.today()
+    delta = datetime.timedelta(days)
+    start = today - delta
+    startday = start.strftime("%d")
+    startmonth = start.strftime("%b")
+    startyear = start.strftime("%Y")
+    nowday = today.strftime("%d")
+    nowmonth = today.strftime("%b")
+    nowyear = today.strftime("%Y")
+    url = "http://www.in.gov/apps/idem/oe/idem_oe_order?"
+    url += "company_name=&case_number=&old_case_number=&county=" + county
+    url += "&media=All&type=0&start_month=" + startmonth + "&start_day=" + startday
+    url += "&start_year=" + startyear + "&end_month=" + nowmonth + "&end_day=" + nowday
+    url += "&end_year=" + nowyear + "&page=T&action=Search"
+    print url
+    page = urllib2.urlopen(url, timeout=100).read()
+    filename = today.isoformat() + "_" + county + ".html"
+    filepath = os.path.join(directory, filename)
+    open(filepath, "w").write(page)
+    output = []
+    rows = page.split("<TR>")[1:]
+    rows = [x for x in rows if '<TD ALIGN="CENTER"' in x]
+    for row in rows:
+        city = row.split('<TD>&nbsp;<font size="-1">')[2].split("<")[0]
+        company = row.split('<font size="-1">', 1)[1].split("<")[0]
+        if "href" in row:
+            actionurl = row.split('<a href="')[1].split('"')[0]
+            basefilename = actionurl.split("/")[-1]
+            type = actionurl.split("/")[-2]
+            filename = company + "_" + city + "_" + type + "_" + basefilename
+        else:
+            actionurl = ""
+            filename = ""
+        output.append((city, company, actionurl, filename))
+        if not filename:
+            continue
+        already = set(os.listdir(directory))
+        if filename not in already:
+            print filename
+            filepath = os.path.join(directory, filename)
+            actionpage = urllib2.urlopen(actionurl, timeout=100).read()
+            open(filepath, "w").write(actionpage)
+            updates.append((today.isoformat(), filename))
+    if updates:
+        tsv = "\n".join(["\t".join(x) for x in updates])
+        writefile = open(os.path.join(directory, "updates_" + today.isoformat() + ".txt"), "w")
+        with writefile:
+            writefile.write(tsv)
+    return updates
+
+
+# INND
+
+class CaseUpdater:
+    def __init__(self):
+        pass
+
+
+def fetch_daily_INND():
+    new = collections.defaultdict(list)
+    alerts = alerts_from_file()
+    alerted = []
+    url = "https://ecf.innd.uscourts.gov/cgi-bin/rss_outside.pl"
+    directory = innddir
+    rss = urllib2.urlopen(url, timeout=100).read()
+    items = rss.split("<item>")[1:]
+    items = [x.split("</item>")[0] for x in items]
+    items.reverse()  # put in chron order
+    for item in items:
+        for alert in alerts:
+            if alert in item:
+                print "***" + alert
+                print item
+                alerted.append(alert)
+        title = item.split("<title>")[1].split("</title>")[0]
+        title = unescape(title)
+        casenum = title.split(" ")[0]
+        caselink = item.split("<link>")[1].split("</link>")[0]
+        pubdate = item.split("<pubDate>")[1].split("</pubDate>")[0]
+        pubdate = unescape(pubdate)
+        description = item.split("<description>", 1)[1].split("</description>")[0]
+        description = unescape(description)
+        if "<a" in description:
+            entrynum = description.split("<a")[1].split(">", 1)[1].split("</a>")[0]
+        else:
+            entrynum = ""
+        if "<guid" in item:
+            guid = item.split("<guid")[1].split(">", 1)[1].split("</guid>")[0]
+            guid = unescape(guid)
+        else:
+            guid = ""
+        new[casenum].append((title, caselink, pubdate, description, entrynum, guid))
+    changed = process_case_updates(new)
+    return changed, alerted
+
+
+def alerts_from_file(filepath=False):
+    if filepath is False:
+        filepath = os.path.join(innddir, "alertme.txt")
+    alertfile = open(filepath)
+    alerts = []
+    for line in alertfile:
+        line = line.split("#")[0]
+        line = line.strip()
+        if not line:
+            continue
+        alerts.append(line)
+    alerts = list(set(alerts))
+    alerts.sort()
+    return alerts
+
+
+def process_alerts(directory=innddir):
+    oho = []
+    filepath = os.path.join(directory, "alertme.txt")
+    alerts = alerts_from_file()
+    for line in alerts:
+        line = line.split("#")[0]
+        line = line.strip()
+        if not line:
+            continue
+        if line in str(os.listdir(directory)):
+            oho.append(line)
+    return oho
+
+
+def process_case_updates(new, directory=innddir):
+    filenames = [casenum + ".html" for casenum in new.keys()]
+    already = set(os.listdir(directory))
+    changed = []
+    for n in filenames:  # creating new files for cases not previously logged
+        path = os.path.join(directory, n)
+        key = n.replace(".html", "")
+        title, caselink, pubdate, description, entrynum, guid = new[key][0]
+        if n in already:
+            html = open(path).read()
+        else:
+            html = create_case_HTML(title, caselink)
+        original = str(html)
+        for title, caselink, pubdate, description, entrynum, guid in new[key]:
+            newrow = create_case_row((title, caselink, pubdate, description, entrynum, guid))
+            if newrow in html:
+                continue
+            html = add_row_to_table(html, newrow)
+        if original != html:
+            open(path, "w").write(html)
+            changed.append(n)
+    return changed
+
+
+def create_case_HTML(title, link):
+    html = "<html><body>%s%s</body></html>"
+    header = '\n<h2><a href="%s">%s</a></h2>\n' % (link, title)
+    table = '\n\n<table width="100%">\n<tr>\n<th width="15%" align ="left">Time</th>'
+    table += '<th width="10%" align="left">Entry</th>'
+    table += '<th align="left">Description</th></tr>\n</table>'
+    output = html % (header, table)
+    return output
+
+
+def create_case_row(casetuple):
+    newrow = "<tr><td>%s</td><td>%s</d><td>%s</td></tr>\n"
+    title, caselink, pubdate, description, entrynum, guid = casetuple
+    firstcell = '<a href="%s">%s</a>' % (guid, pubdate)
+    secondcell = entrynum
+    thirdcell = description
+    newrow = newrow % (firstcell, secondcell, thirdcell)
+    return newrow
+
+
+def add_row_to_table(html, newrow):
+    html = html.replace("</table>", "\n" + newrow + "</table>")
+    return html
+
+
+def unescape(s):  # ex https://wiki.python.org/moin/EscapingXml
+    want_unicode = False
+    if isinstance(s, unicode):
+        s = s.encode("utf-8")
+        want_unicode = True
+    list = []
+    p = xml.parsers.expat.ParserCreate("utf-8")
+    p.buffer_text = True
+    p.returns_unicode = want_unicode
+    p.CharacterDataHandler = list.append
+    p.Parse("<e>", 0)
+    p.Parse(s, 0)
+    p.Parse("</e>", 1)
+    es = ""
+    if want_unicode:
+        es = u""
+    return es.join(list)
 
 
 ### permits
@@ -957,6 +1170,16 @@ def get_sites_with_activity(zip, sincedate=datetime.date(2017, 1, 1)):
     return sites_with_activity
 
 
+def get_enforcement_address(page):
+    page = re.sub("\s+", " ", page)
+    if " located at " not in page:
+        return False
+    addr = page.split(" located at ")[1]
+    addr = addr.split(" in ")[0].split(",")[0].split(".")[0]
+    addr = re.sub("<.+?>", "", addr)
+    return addr
+
+
 def addrs_are_same(add1, add2):
     if add1 == add2:
         return True
@@ -1042,7 +1265,6 @@ def get_total_from_page(page):
     pattern2 = "Items 1-\d+ of (\d+)"
     pattern3 = "Found (\d+) items"
     done = False
-    total = 0
     for pattern in [pattern1, pattern2, pattern3]:
         try:
             total = re.search(pattern, page).group(1)
