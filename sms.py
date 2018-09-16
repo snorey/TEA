2ccdc68fae525ed1e039ff81781e3eef9994bc9b
snorey <samueljhenderson@gmail.com>
2018-09-16 00:56:42 -0500

    create

---
+++ sms.py
@@ -0,0 +1,27 @@
+from twilio.rest import Client
+
+from idem_settings import twilio_number, twilio_sid, twilio_token, notify_sid
+
+
+def send_message(to, content=""):
+    client = Client(twilio_sid, twilio_token)
+    if not to.startswith("+"):
+        to = "+" + to
+    client.messages.create(to=to, from_=twilio_number, body=content)
+
+
+def get_bindings():
+    client = Client(twilio_sid, twilio_token)
+    notify = client.notify.services(notify_sid)
+    bindings = notify.bindings.list()
+    return bindings
+
+
+def send_message_by_tag(content, tag=None):
+    bindings = get_bindings()
+    for b in bindings:
+        if tag is not None:
+            if tag not in b.tags:
+                continue
+        number = b.address
+        send_message(number, content)
