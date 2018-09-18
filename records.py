c2539a4f1e23eaa3fea8f79af1be4f17ff74e658
snorey <samueljhenderson@gmail.com>
2018-09-18 14:28:17 -0500

    do_cron, tweaks to page parsing

--- records.py
+++ records.py
@@ -41,7 +41,6 @@ class Fetcher:
     def break_page_into_pieces(self, page=""):
         if not page:
             page = self.page
-#        splitter = '"SearchResultsHeading"'
         splitter = '<table width="100%" border="0" cellpadding="0" cellspacing="0">'
         pieces = page.split(splitter)[1:]
         pieces = [x.split("</td>")[0] for x in pieces]
@@ -76,7 +75,16 @@ class Fetcher:
             pieces = self.pieces
         self.notices = []
         for p in pieces:
-            self.notices.append(Notice(from_input=p))
+            self.validate_and_add_notice(p)
+
+    def validate_and_add_notice(self, piece):
+        whether_link = bool(get_first_link_in_text(piece))
+        whether_body = bool(get_body_of_notice(piece))
+        if not all([whether_link, whether_body]):
+            return
+        else:
+            new_notice = Notice(from_input=piece)
+            self.notices.append(new_notice)
 
     @staticmethod
     def get_page_path():
@@ -84,6 +92,26 @@ class Fetcher:
         filename = "public_notices_%s.html" % today
         path = os.path.join(idem_settings.noticedir, filename)
         return path
+
+
+def get_first_link_in_text(text):
+    pieces = text.split('<a href="', 1)
+    if len(pieces) < 2:
+        return None
+    else:
+        link = pieces[1]
+        link = link.split('"')[0]
+        return link
+
+
+def get_body_of_notice(text):
+    pieces = text.split('<img', 1)
+    if len(pieces) < 2:
+        return
+    else:
+        body_chunk = pieces[1]
+        body_chunk = body_chunk.split("<br>")[1]
+        return body_chunk
 
 
 class Notice(tea_core.Document):
@@ -106,7 +134,7 @@ class Notice(tea_core.Document):
     def process_input(self, content):
         domain = "http://in.mypublicnotices.com"
         self.content = content
-        link = content.split('<a href="', 1)[1].split('"')[0]
+        link = get_first_link_in_text(content)
         self.url = domain + link
         body_chunk = content.split('<img')[1].split("<br>")[1]
         if "<B>" in body_chunk:
@@ -115,6 +143,15 @@ class Notice(tea_core.Document):
         else:
             self.title = content.split('<img')[0]
             self.body = content
+        self.get_newspaper_info(content=content)
+        worthy_words = ["HEARING", "MEETING"]
+        if any([x in self.title for x in worthy_words]):
+            self.interesting = True
+        if self.interesting is True:
+            # do something
+            pass
+
+    def get_newspaper_info(self, content):
         if "Appeared in:" in content:
             newspaper_chunk = content.split("Appeared in:")[1].split("</td>")[0]
             self.newspaper = newspaper_chunk.split("<i>")[1].split("</i>")[0]
@@ -123,11 +160,6 @@ class Notice(tea_core.Document):
                 new_date = datetime.datetime.strptime(date_string, "%m/%d/%Y")
                 self.dates.append(new_date)
             # "Appeared in: <b><i>The Times</i></b> on 01/12/2018 and 01/19/2018<"
-        worthy_words = ["HEARING", "MEETING"]
-        if any([x in self.title for x in worthy_words]):
-            self.interesting = True
-        if self.interesting is True:
-            pass
 
 
 def get_top_page_number(page):
@@ -139,6 +171,11 @@ def get_top_page_number(page):
     return top_page_number
 
 
-def daily_action():
+def do_cron():
     fetcher = Fetcher()
     fetcher.fetch_all()
+    fetcher.save()
+
+
+if __name__ == "__main__":
+    do_cron()
