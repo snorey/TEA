f338e6e6c05aefe6cc61f097c819df988b370996
snorey <samueljhenderson@gmail.com>
2018-09-21 02:31:25 -0500

    unit tests for Document class

--- test_idem.py
+++ test_idem.py
@@ -1,5 +1,45 @@
-import unittest
+import datetime
 import idem
+import unittest
+
+
+class DocumentTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.docid = "101"
+        self.facility = "argle"
+        self.date1 = datetime.date(2018, 1, 1)
+        self.date2 = datetime.date(2018, 10, 1)
+
+    def tearDown(self):
+        pass
+
+    def test_same_filenames_are_equal(self):
+        doc1 = idem.Document(filename="foo")
+        doc2 = idem.Document(filename="bar")
+        self.assertNotEqual(doc1, doc2)
+
+    def test_empty_filenames_are_unequal(self):
+        doc1 = idem.Document(filename="", id=self.docid)
+        doc2 = idem.Document(filename="", id="10201")
+        self.assertNotEqual(doc1, doc2)
+
+    def test_earlier_file_date_is_less_than_later(self):
+        doc1 = idem.Document(file_date=self.date1)
+        doc2 = idem.Document(file_date=self.date2)
+        self.assertLess(doc1, doc2)
+
+    def test_identity_is_filename(self):
+        doc = idem.Document(id=self.docid, facility=self.facility)
+        self.assertEqual(doc.identity, doc.filename)
+
+    def test_filename_escapes_slashes(self):
+        doc = idem.Document(id=self.docid, program="Foo/Bar", type="Meh")
+        self.assertFalse("/" in doc.filename)
+
+    def test_latest_date_returns_filedate_if_crawldate_is_null(self):
+        doc = idem.Document(file_date=self.date1)
+        self.assertEqual(doc.latest_date, self.date1)
 
 
 class DocumentCollectionTestCase(unittest.TestCase):
@@ -82,10 +122,11 @@ class FacilityCollectionTestCase(unittest.TestCase):
     def test_fc_non_document_raises_TypeError(self):
         self.assertRaises(TypeError, self.collection.append, "document")
 
-    def test_fc_updates_types(self):
-        self.collection.append(self.new_fac)
-        self.assertTrue(self.new_fac.vfc_id in self.collection.iddic.keys())
-        self.assertTrue(self.new_fac.vfc_name in self.collection.namedic.keys())
+    def test_fc_updates_names_and_ids(self):
+        new_fac = idem.Facility(id="300", vfc_name="Argle Bargle")
+        self.collection.append(new_fac)
+        self.assertTrue(new_fac.vfc_id in self.collection.iddic.keys())
+        self.assertTrue(new_fac.vfc_name in self.collection.namedic.keys())
 
     def test_fc_does_not_validate_duplicate(self):
         self.assertFalse(self.collection.validate_item(self.fac100))
